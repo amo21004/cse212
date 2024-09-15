384ad1e1de5afa60f00c827d614e07a49debdb52
Naif Amoodi <naif@rhyzz.com>
2024-09-15 23:27:48 +0530

    Demonstrating speed of fixed and dynamic arrays

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Diagnostics;
 
 public class Program
 {
@@ -8,6 +8,57 @@ public class Program
         // with any code or ideas you have that do not directly apply to
         // one of your projects.
 
-        Console.WriteLine("Hello Sandbox World!");
+        var maximumItems = 100000;
+
+        for(int i = 1; i <= 5; i++) {
+            Console.WriteLine("Test #" + i);
+
+            var fixedArrayResult = FixedArray(maximumItems);
+
+            PrintResult("fixed", fixedArrayResult);
+
+            var dynamicArrayResult = DynamicArray(maximumItems);
+
+            PrintResult("dynamic", dynamicArrayResult);
+
+            Console.WriteLine();
+        }
+    }
+
+    static Stopwatch FixedArray(int maximumItems)
+    {
+        var sw = Stopwatch.StartNew();
+
+        int[] fixedArray = new int[maximumItems];
+
+        for (int i = 0; i < maximumItems; i++)
+        {
+            fixedArray[i] = i;
+        }
+
+        sw.Stop();
+
+        return sw;
+    }
+
+    static Stopwatch DynamicArray(int maximumItems)
+    {
+        var sw = Stopwatch.StartNew();
+
+        List<int> dynamicArray = new List<int>();
+
+        for (int i = 0; i < maximumItems; i++)
+        {
+            dynamicArray.Add(i);
+        }
+
+        sw.Stop();
+
+        return sw;
+    }
+
+    static void PrintResult(string what, Stopwatch sw)
+    {
+        Console.WriteLine("Time for populating a " + what + " array: " + sw.Elapsed.TotalMilliseconds + " seconds");
     }
 }
