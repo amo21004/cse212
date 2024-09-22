5dc3809ae21b88baf409c6e0d4a2709a12466e42
Naif Amoodi <naif@rhyzz.com>
2024-09-22 14:58:06 +0530

    W02 Interview Question Demo

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 
 public class Program
 {
+    /*
     static void Main(string[] args)
     {
         // This project is here for you to use as a "Sandbox" to play around
@@ -60,5 +61,63 @@ public class Program
     static void PrintResult(string what, Stopwatch sw)
     {
         Console.WriteLine("Time for populating a " + what + " array: " + sw.Elapsed.TotalMilliseconds + " seconds");
+    }
+    */
+
+    // W02: Interview question
+    static void Main(string[] args)
+    {
+        List<Seed> seeds = new List<Seed>{
+            new Seed("Pumpkins", 5),
+            new Seed("Peas", 25),
+            new Seed("Strawberries", 10),
+            new Seed("Tomatoes", 10),
+            new Seed("Peas", 15),
+            new Seed("Strawberries", 20)            
+        };
+
+        Stack<Seed> pilesOfSeeds = new Stack<Seed>();
+
+        Random randomIndexGenerator = new Random();
+
+        Console.WriteLine("Piles of Seeds Collected:\n\n");
+
+        while(seeds.Count > 0) {
+            var index = randomIndexGenerator.Next(0, seeds.Count);
+
+            var seed = seeds[index];
+
+            seeds.RemoveAt(index);
+
+            pilesOfSeeds.Push(seed);
+
+            Console.WriteLine(seed);
+        }
+
+        Console.WriteLine("\n\nPlanting them now!\n\n");
+
+
+        while(pilesOfSeeds.Count > 0) {
+            var seed = pilesOfSeeds.Pop();
+
+            Console.WriteLine($"Planting: {seed}");
+        }
+    }
+}
+
+internal class Seed
+{
+    internal string Name { get; set; }
+    internal int Quantity { get; set; }
+
+    internal Seed(string name, int quantity)
+    {
+        Name = name;
+        Quantity = quantity;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} ({Quantity})";
     }
 }
---
+++ w02interview/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Seed> seeds = new List<Seed>{
+            new Seed("Pumpkins", 5),
+            new Seed("Peas", 25),
+            new Seed("Strawberries", 10),
+            new Seed("Tomatoes", 10),
+            new Seed("Peas", 15),
+            new Seed("Strawberries", 20)            
+        };
+
+        Stack<Seed> pilesOfSeeds = new Stack<Seed>();
+
+        Random randomIndexGenerator = new Random();
+
+        Console.WriteLine("Piles of seeds collected from the garden:\n\n");
+
+        while(seeds.Count > 0) {
+            var index = randomIndexGenerator.Next(0, seeds.Count);
+
+            var seed = seeds[index];
+
+            seeds.RemoveAt(index);
+
+            pilesOfSeeds.Push(seed);
+
+            Console.WriteLine(seed);
+        }
+
+        Console.WriteLine("\n\nPlanting them now!\n\n");
+
+
+        while(pilesOfSeeds.Count > 0) {
+            var seed = pilesOfSeeds.Pop();
+
+            Console.WriteLine($"Planting: {seed}");
+        }
+    }
+}
+
+internal class Seed
+{
+    internal string Name { get; set; }
+    internal int Quantity { get; set; }
+
+    internal Seed(string name, int quantity)
+    {
+        Name = name;
+        Quantity = quantity;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} ({Quantity})";
+    }
+}
